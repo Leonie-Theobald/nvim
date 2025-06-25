3e445c0d3425c49131cf331da47965eeffb984b1
leonie-theobald <leonie.theobald@mercedes-benz.com>
2025-06-25 13:11:48 +0200

    Mark 80 to 100 columns

--- lua/leonie/basic.lua
+++ lua/leonie/basic.lua
@@ -8,5 +8,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 
+vim.cmd("set colorcolumn=81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100")
+vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=235 })
+
+
 vim.opt.spelllang="en"
 vim.opt.spell = true
