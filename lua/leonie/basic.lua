64be9ec4e446d1347064c9574fd7550a5a31c0fe
leonie-theobald <leonie.theobald@mercedes-benz.com>
2025-06-30 11:11:13 +0200

    Added comments for better description

--- lua/leonie/basic.lua
+++ lua/leonie/basic.lua
@@ -1,16 +1,19 @@
 print("Setup basic.lua")
 
+-- suppress start screen
 vim.opt.shortmess:append("sI")
 
 vim.g.mapleader = " "
 
+-- set options for line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 
+-- mark the 80 and 100 column
 vim.cmd("set colorcolumn=81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100")
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=235 })
 
-
+-- set language options
 vim.opt.spelllang="en"
 vim.opt.spell = true
