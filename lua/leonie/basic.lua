9709addc7420449a00f72aecc905caa01b7497b3
Leonie Theobald <theobaldleonie@web.de>
2026-06-21 00:27:26 +0200

    Automatic line wrap at 100

--- lua/leonie/basic.lua
+++ lua/leonie/basic.lua
@@ -14,6 +14,10 @@ vim.opt.signcolumn = "yes"
 vim.cmd("set colorcolumn=81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100")
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=235 })
 
+-- wrap text at 100
+vim.opt.textwidth = 100
+vim.opt.formatoptions:append("t")
+
 -- set language options
 vim.opt.spelllang="en"
 vim.opt.spell = true
